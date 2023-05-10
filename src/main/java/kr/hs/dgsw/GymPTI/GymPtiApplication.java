9f8500d60614897cfe48882a7c0898260c3d0e5a
rlawhddbs <savagestrikekr@gmail.com>
2023-05-10 17:07:18 +0900

    :tada: :: GymPTI Server Project Start!

---
+++ src/main/java/kr/hs/dgsw/GymPTI/GymPtiApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.GymPTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPtiApplication.class, args);
+	}
+
+}
