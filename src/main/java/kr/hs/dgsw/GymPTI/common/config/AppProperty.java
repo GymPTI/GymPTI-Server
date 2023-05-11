c673ab14635a8dc47ffa1e35413ad63da8676e87
rlawhddbs <savagestrikekr@gmail.com>
2023-05-11 20:29:43 +0900

    :wrench: :: Add Config & Annotation

---
+++ src/main/java/kr/hs/dgsw/GymPTI/common/config/AppProperty.java
@@ -0,0 +1,16 @@
+package kr.hs.dgsw.GymPTI.common.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppProperty {
+
+    private String accessSecretKey;
+
+    private String refreshSecretKey;
+
+}
---
+++ src/main/java/kr/hs/dgsw/GymPTI/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package kr.hs.dgsw.GymPTI.common.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableJpaAuditing
+public class JpaConfig {
+}
